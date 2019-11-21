e7236d9fa6eda10a4120de4734e9d2ad3cbbd45a
Gustavo Sousa <gustavosousa@jeporu.com>
2019-11-21 12:44:27 -0300

    Use of react-router-bootstrap in navigation

--- frontend/src/scripts/components/container/Sidebar.jsx
+++ frontend/src/scripts/components/container/Sidebar.jsx
@@ -1,30 +1,22 @@
-import React from "react";
-// import ReactDOM from "react-dom";
+import React, {Component} from "react";
 import ListGroup from "react-bootstrap/ListGroup"
-import {NavLink} from "react-router-dom";
+import {LinkContainer} from 'react-router-bootstrap'
 
-export default class Sidebar extends React.Component {
+export default class Sidebar extends Component {
     render() {
         return (
             <div className="bg-light border-right" id="sidebar-wrapper">
                 <div className="sidebar-heading">Start Bootstrap</div>
                 <ListGroup variant="flush">
-                    {/*<ListGroup.Item action active href="#">Início</ListGroup.Item>*/}
-                    <NavLink exact to="/"
-                          className="list-group-item list-group-item-action"
-                          activeClassName="active">
-                        Início
-                    </NavLink>
-                    <NavLink to="/book"
-                          className="list-group-item list-group-item-action"
-                          activeClassName="active">
-                        Livros
-                    </NavLink>
-                    <NavLink to="/person"
-                          className="list-group-item list-group-item-action"
-                          activeClassName="active">
-                        Pessoas
-                    </NavLink>
+                    <LinkContainer exact to="/">
+                        <ListGroup.Item action>Início</ListGroup.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/book">
+                        <ListGroup.Item action>Livros</ListGroup.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/person">
+                        <ListGroup.Item action>Pessoas</ListGroup.Item>
+                    </LinkContainer>
                 </ListGroup>
             </div>
         )
