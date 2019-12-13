ebb9898f76963121f9f3ec250375c62bd28dde4f
Gustavo Sousa <gustavosousa@jeporu.com>
2019-12-13 17:12:33 -0300

    Making <ListGroup.Item> clickable

--- frontend/src/components/container/BookListItem.jsx
+++ frontend/src/components/container/BookListItem.jsx
@@ -1,20 +1,24 @@
 import React, {Component} from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import {Link} from "react-router-dom";
+import {LinkContainer} from 'react-router-bootstrap'
 
 export default class BookListItem extends Component {
     render() {
         const item = this.props.item;
+        const toUrl = `/book/${item.id}/`;
         return (
-            <ListGroup.Item className="d-sm-flex justify-content-sm-between">
-                <div>
-                    <p className="m-0"><strong>{item.title}</strong></p>
-                    <p className="m-0 font-weight-light text-secondary">{item.original_title}</p>
-                </div>
-                <div className="pt-2 pb-1 py-sm-0 pl-sm-3">
-                    <Link to={`/book/${item.id}/`} className="btn btn-xs btn-info">Editar</Link>
-                </div>
-            </ListGroup.Item>
+            <LinkContainer to={toUrl}>
+                <ListGroup.Item className="d-sm-flex justify-content-sm-between cursor-pointer">
+                    <div>
+                        <p className="m-0"><strong>{item.title}</strong></p>
+                        <p className="m-0 font-weight-light text-secondary">{item.original_title}</p>
+                    </div>
+                    <div className="pt-2 pb-1 py-sm-0 pl-sm-3">
+                        <Link to={toUrl} className="btn btn-xs btn-info">Editar</Link>
+                    </div>
+                </ListGroup.Item>
+            </LinkContainer>
         )
     }
 }
--- frontend/src/components/container/PublisherListItem.jsx
+++ frontend/src/components/container/PublisherListItem.jsx
@@ -1,19 +1,23 @@
 import React, {Component} from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import {Link} from "react-router-dom";
+import {LinkContainer} from "react-router-bootstrap";
 
 export default class PublisherListItem extends Component {
     render() {
         const item = this.props.item;
+        const toUrl = `/publisher/${item.id}/`;
         return (
-            <ListGroup.Item className="d-sm-flex justify-content-sm-between">
-                <div>
-                    <p className="m-0"><strong>{item.name}</strong></p>
-                </div>
-                <div className="pt-2 pb-1 py-sm-0 pl-sm-3">
-                    <Link to={`/publisher/${item.id}/`} className="btn btn-xs btn-info">Editar</Link>
-                </div>
-            </ListGroup.Item>
+            <LinkContainer to={toUrl}>
+                <ListGroup.Item className="d-sm-flex justify-content-sm-between cursor-pointer">
+                    <div>
+                        <p className="m-0"><strong>{item.name}</strong></p>
+                    </div>
+                    <div className="pt-2 pb-1 py-sm-0 pl-sm-3">
+                        <Link to={toUrl} className="btn btn-xs btn-info">Editar</Link>
+                    </div>
+                </ListGroup.Item>
+            </LinkContainer>
         )
     }
 }
