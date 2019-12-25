59ec9673e2502f4dbee47eb311d5d7d529d57425
Gustavo Sousa <gustavosousa@jeporu.com>
2019-12-25 18:25:59 -0300

    Organizing fixtures in conftest file

--- backend/book/tests/conftest.py
+++ backend/book/tests/conftest.py
@@ -1,39 +1,52 @@
 import pytest
-from book.models import AgeClassification, Book, PersonProfile, PersonType, Publisher, TextualClassification
 
 
 @pytest.fixture
 def supply_book_publisher():
+    from book.models import Publisher
+
     return Publisher(name='name 1')
 
 
 @pytest.fixture
 def supply_book_age_classification():
+    from book.models import AgeClassification
+
     return AgeClassification(name='name 1')
 
 
 @pytest.fixture
 def supply_book_textual_classification():
+    from book.models import TextualClassification
+
     return TextualClassification(name='name 1')
 
 
 @pytest.fixture
 def supply_book_person_profile():
+    from book.models import PersonProfile
+
     return PersonProfile(first_name='first_name 1', last_name='last_name 1')
 
 
 @pytest.fixture
 def supply_book_person_profile_2():
+    from book.models import PersonProfile
+
     return PersonProfile(first_name='first_name 2', last_name='last_name 2')
 
 
 @pytest.fixture
 def supply_book_person_type():
+    from book.models import PersonType
+
     return PersonType(name='type 1')
 
 
 @pytest.fixture
 def supply_book(supply_book_publisher, supply_book_age_classification, supply_book_textual_classification):
+    from book.models import Book
+
     supply_book_publisher.save()
     supply_book_age_classification.save()
     supply_book_textual_classification.save()
@@ -44,10 +57,8 @@ def supply_book(supply_book_publisher, supply_book_age_classification, supply_bo
     return book
 
 
-# @pytest.fixture
-# def supply_book_person(supply_book, supply_book_person_profile, supply_book_person_type):
-#     return BookPerson(
-#         book=supply_book,
-#         person=supply_book_person_profile,
-#         type=supply_book_person_type
-#     )
+@pytest.fixture
+def supply_book_person(supply_book, supply_book_person_profile, supply_book_person_type):
+    from book.models import Person
+
+    return Person(book=supply_book, person=supply_book_person_profile, type=supply_book_person_type)
